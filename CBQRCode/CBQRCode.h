69b9cbfd76a00baff7d187d458f35b57332b7ab6
消失的旅人 <869412057@qq.com>
2016-03-08 19:38:39 +0800

    init project

---
+++ CBQRCode/CBQRCode.h
@@ -0,0 +1,19 @@
+//
+//  CBQRCode.h
+//  CBQRCode
+//
+//  Created by xsdlr on 16/3/8.
+//  Copyright © 2016年 xsdlr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CBQRCode.
+FOUNDATION_EXPORT double CBQRCodeVersionNumber;
+
+//! Project version string for CBQRCode.
+FOUNDATION_EXPORT const unsigned char CBQRCodeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CBQRCode/PublicHeader.h>
+
+
---
+++ CBQRCodeTests/CBQRCodeTests.swift
@@ -0,0 +1,19 @@
+//
+//  CBQRCodeTests.swift
+//  CBQRCodeTests
+//
+//  Created by xsdlr on 16/3/8.
+//  Copyright © 2016年 xsdlr. All rights reserved.
+//
+
+import Nimble
+import Quick
+@testable import CBQRCode
+
+class CBQRCodeTests: QuickSpec {
+    override func spec() {
+        describe("first test") {
+            
+        }
+    }
+}
